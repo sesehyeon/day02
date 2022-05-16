3c5449a9cd6c4cbd4f450dd6ecba067b5a06032e
sehyeon <sehyeon111@naver.com>
2022-05-16 16:34:20 +0900

    추가 수정본

--- src/day02/test02.java
+++ src/day02/test02.java
@@ -1,11 +1,21 @@
 package day02;
 
-public class test02 {
+import java.util.Scanner;
+
+public class Test02 {
 
 	public static void main(String[] args) {
-		int i = 0;
-		if (1<i) {
+		
+		
+		Scanner scan =new Scanner(System.in);
+		System.out.println("숫자 하나를 입력하시오: ");
+		int num=scan.nextInt();
+		
+		if(num%2==1) {
+			System.out.println("홀수입니다");
 			
+		}else {
+			System.out.println("짝수 입니다.");
 		}
 		
 
